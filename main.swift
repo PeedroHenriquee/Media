e1874925845ab8229386b5fd782a4c1cba6041d1
Pedro Henrique <83087869+PeedroHenriquee@users.noreply.github.com>
2023-03-25 20:39:43 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+//Faça um programa que calcule a média de três notas e exibe o resultado no console.
+
+
+let nota1:Float = 10
+let nota2:Float = 20
+let nota3:Float = 40
+
+let media = (nota1 + nota2 + nota3)/2
+
+print (media)
